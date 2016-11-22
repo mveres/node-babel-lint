f4d46bb110161939a1bdf4ddb1c4d29330e1aefb
mveres <veres85@yahoo.com>
2016-11-22 10:27:00 +0200

    added electron packager

--- client/app/app.js
+++ client/app/app.js
@@ -2,24 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ipcRenderer } from 'electron';
 
-const f1 = () => new Promise(resolve => setTimeout(() => resolve(43), 1000));
-const send = async () => {
-  ipcRenderer.send('asynchronous-message', 'ping');
-
-  const r = await f1();
-  console.log(r);
-};
-
-ipcRenderer.on('asynchronous-reply', (event, arg) => {
-  console.log(arg); // prints "pong"
-});
+const connect = options => ipcRenderer.send('connect', options);
+const send = data => ipcRenderer.send('send', data);
 
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { host: '192.168.1.185', port: '1109' };
+
+    ipcRenderer.on('connect-reply', (event, { connected }) => {
+      this.setState({ connected });
+    });
+  }
+
   render() {
     return (
       <div>
-        <button onClick={ send } />
+        <label htmlFor="host">
+          Host:
+          <input type="text"
+                 name="host"
+                 onChange={ event => this.setState({ host: event.target.value }) }
+                 value={ this.state.host } />
+        </label>
+        <label htmlFor="port">
+          Port:
+          <input type="text"
+                 name="port"
+                 onChange={ event => this.setState({ port: event.target.value }) }
+                 value={ this.state.port } />
+        </label>
+        <div> { this.state.connected ? 'Connected' : 'Not Connected'} </div>
+        <button onClick={ () => connect(this.state) } disabled={ !!this.state.connected }>Connect</button>
+        <button onClick={ () => send('') } disabled={ !this.state.connected }>Send</button>
       </div>
     );
   }
